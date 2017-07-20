39b36c300a994f9d67216744420123118a8052d2
Alexandre FERNANDES <4lex.fernandes@gmail.com>
2017-07-20 16:44:19 +0200

    ajout fichier inscription.php et modif function.inc.php (verif mdp)

--- inc/function.inc.php
+++ inc/function.inc.php
@@ -1,5 +1,29 @@
 <?php
 
+// fonction de check du password
+// cette fonction renvoi FALSE si le mot de passe fait moins de 8 caractères, s'il ne contient pas au moins 1 majuscule, 1 minuscule et 1 caractère spécial défini dans l'expression régulière => '/[!@#$%^&*()\-_=+{};:,<.>]/'. Si tout est ok => TRUE
+// Adapter au besoin
+	
+function checkPassword($password) {
+   $reg1='/[A-Z]/';  // majuscule
+   $reg2='/[a-z]/';  // minuscule
+   //$reg3='/[!@#$%^&*()\-_=+{};:,<.>]/';  // caractère spécial
+   $reg4='/[0-9]/';  // chiffre
+
+   // preg_match_all() compte le nombre d'occurence d'une expression régulière et place chaque occurence dans un tableau array défini en 3 ème argument (ici $tab) et renvoi un INT représentant le nombre d'occurence total.
+   if(preg_match_all($reg1,$password, $tab)<1) return FALSE; // check si au moins une majuscule
+
+   if(preg_match_all($reg2,$password, $tab)<1) return FALSE; // check si au moins une minuscule
+
+   //if(preg_match_all($reg3,$password, $tab)<1) return FALSE; // check si au moins un caractère spécial
+
+   if(preg_match_all($reg4,$password, $tab)<1) return FALSE; // check si au moins un chiffre
+
+   if(mb_strlen($password, 'utf-8')<8) return FALSE; // check si taille inférieure à 8
+   
+   return TRUE;
+}
+
 // fonction pour savoir si un utilisateur est connecté
 function utilisateur_est_connecte()
 {
--- inscription.php
+++ inscription.php
@@ -4,7 +4,8 @@ require("inc/init.inc.php");
 // vérification si l'utilisateur est connecté, sinon redirection
 if(utilisateur_est_connecte())
 {
-    header("location:profil.php");
+    // redirection à activer vers page d'accueil, quand elle existera
+    // header("location:profil.php");
 }
 
 // déclaration des variables vide pour affichage dans les values du formulaire
@@ -13,14 +14,13 @@ $mdp = "";
 $nom = "";
 $prenom = "";
 $email = "";
-$sexe = "";
-$ville = "";
-$cp = "";
-$adresse = "";
+$civilite = "";
 
-if(isset($_POST['pseudo']) && isset($_POST['mdp']) && isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['email']) && isset($_POST['sexe']) && isset($_POST['ville']) && isset($_POST['cp']) && isset($_POST['adresse'])) // personne n'a manipulé notre formulaire
+
+// controle de la présence des champs dans le formulaire d'inscription
+if(isset($_POST['pseudo']) && isset($_POST['mdp']) && isset($_POST['nom']) && isset($_POST['prenom']) && isset($_POST['email']) && isset($_POST['civilite']))
 {
-  // attribution des nouvelles valeurs: cf. value="" dans le formulaire
+  // attribution des nouvelles valeurs
   // les champs se "souviennent" des valeurs si on recharge la page.
   // si le formulaire a été validé, on place dans ces variables les saisies correspondantes
   $pseudo = $_POST['pseudo'];
@@ -28,50 +28,22 @@ if(isset($_POST['pseudo']) && isset($_POST['mdp']) && isset($_POST['nom']) && is
   $nom = $_POST['nom'];
   $prenom = $_POST['prenom'];
   $email = $_POST['email'];
-  $sexe = $_POST['sexe'];
-  $ville = $_POST['ville'];
-  $cp = $_POST['cp'];
-  $adresse = $_POST['adresse'];
+  $civilite = $_POST['civilite'];
 
   // variable de contrôle des erreurs
   $erreur = "";
 
-  // contrôle sur la taille du pseudo (entre 4 et 14 caractères inclus)
+//////////////////// PSEUDO //////////////////////////////////////////////
+
+  // contrôle sur la taille du pseudo (entre 4 et 20 caractères inclus)
   $taille_pseudo = iconv_strlen($pseudo);
-  if($taille_pseudo < 4 || $taille_pseudo > 14)
+  if($taille_pseudo < 4 || $taille_pseudo > 20)
   {
-    $message .= '<div class="alert alert-danger" role="alert" style="margin-top: 20px;">Attention, la taille du pseudo est incorrecte.<br />En effet, le pseudo doit avoir entre 4 et 14 caractères inclus</div>';
+    $message .= '<div class="alert alert-danger" role="alert" style="margin-top: 20px;">Attention, la taille du pseudo est incorrecte.<br />En effet, le pseudo doit avoir entre 4 et 20 caractères inclus</div>';
     $erreur = true; // si l'on rentre dans cette condition alors il y a une erreur.
   }
 
-  // contrôle des caractères dans le pseudo (autorisés: a-z A-Z 0-9 _ - .)
-  $verif_caracteres = preg_match('#^[a-zA-Z0-9._-]+$#', $pseudo);
-  /*
-  // preg_match() va vérifier les caractères contenus dans la variable pseudo selon une expression régulière fournie en 1er argument.
-  // renvoie 1 si tout est ok, sinon 0.
-  
-  // expression:
-  // # => permet d'indiquer le début et la fin de l'expression
-  // ^ => indique que la chaîne ($pseudo) ne peut que commencer par ces caractères.
-  // $ => indique que la chaîne ($pseudo) ne peut que finir par ces caractères.
-  // + => indique que les caractères autorisés peuvent apparaître plusieurs fois.
-  // [] => contient les caractères autorisés.
-  */
-  if(!$verif_caracteres && !empty($pseudo)) // si false
-  {
-    // on rentre dans cette condition si $verif_caractère contient 0, cad si il y a des caractères non autorisés.
-    $message .= '<div class="alert alert-danger" role="alert" style="margin-top: 20px;">Attention, caractères non autorisés dans le pseudo.<br />Caractère autorisés: A-Z et 0-9</div>';
-    $erreur = true;
-  }
-
-  // contrôle sur la validité du format de l'email et non vide
-  if(!filter_var($email ,FILTER_VALIDATE_EMAIL) && !empty($email))
-  {
-    $message .= '<div class="alert alert-danger" role="alert" style="margin-top: 20px;">Attention, le format de votre email n\'est pas valide.<br />Veuillez saisir un email valide.</div>';
-    $erreur = true;
-  }
-
-  // contrôle sur la disponibilité du pseudo en BDD
+  // contrôle si le pseudo est unique
   $verif_pseudo = $pdo->prepare("SELECT * FROM membre WHERE pseudo = :pseudo");
   $verif_pseudo->bindParam(':pseudo', $pseudo, PDO::PARAM_STR);
   $verif_pseudo->execute();
@@ -83,7 +55,51 @@ if(isset($_POST['pseudo']) && isset($_POST['mdp']) && isset($_POST['nom']) && is
     $erreur = true;
   }
 
-  // AUTRES CONTROLES - A FAIRE
+//////////////////////////// fin pseudo ///////////////////////////////////
+
+// contrôles sur le mdp
+
+  // contrôle sur la taille du mot de passe (entre 8 et 20 caractères inclus)
+  $taille_mdp = iconv_strlen($mdp);
+  if($taille_mdp < 8 || $taille_mdp > 20)
+  {
+    $message .= '<div class="alert alert-danger" role="alert" style="margin-top: 20px;">Attention, la taille du mot de passe est incorrecte.<br />En effet, le pseudo doit avoir entre 8 et 20 caractères inclus</div>';
+    $erreur = true; // si l'on rentre dans cette condition alors il y a une erreur.
+  }
+
+  // vérificaction de la validité du mot de passe
+  if(!checkPassword($mdp)) 
+  {
+    $message .= '<div class="alert alert-danger" role="alert" style="margin-top: 20px;">Attention, le mot de passe n\'est pas valable.</div>';
+    $erreur = true;
+  }
+
+
+// contrôles su le nom
+  // contrôle sur la taille du nom (entre 1 et 30 caractères inclus)
+  $taille_nom = iconv_strlen($nom);
+  if($taille_nom < 1 || $taille_nom > 30)
+  {
+    $message .= '<div class="alert alert-danger" role="alert" style="margin-top: 20px;">Attention, la taille du nom est incorrecte.<br />En effet, le nom doit avoir entre 1 et 30 caractères inclus</div>';
+    $erreur = true; // si l'on rentre dans cette condition alors il y a une erreur.
+  }
+
+// contrôles sur le prenom
+  // contrôle sur la taille du nom (entre 1 et 30 caractères inclus)
+  $taille_prenom = iconv_strlen($prenom);
+  if($taille_prenom < 1 || $taille_prenom > 30)
+  {
+    $message .= '<div class="alert alert-danger" role="alert" style="margin-top: 20px;">Attention, la taille du prénom est incorrecte.<br />En effet, le nom doit avoir entre 1 et 30 caractères inclus</div>';
+    $erreur = true; // si l'on rentre dans cette condition alors il y a une erreur.
+  }
+
+
+// contrôle sur la validité du format de l'email et non vide
+  if(!filter_var($email ,FILTER_VALIDATE_EMAIL) || empty($email))
+  {
+    $message .= '<div class="alert alert-danger" role="alert" style="margin-top: 20px;">Attention, le format de votre email n\'est pas valide.<br />Veuillez saisir un email valide.</div>';
+    $erreur = true;
+  }
 
   // insertion dans la BDD
   if($erreur !== true) // pas d'erreur dans le remplissage du formulaire
@@ -91,20 +107,17 @@ if(isset($_POST['pseudo']) && isset($_POST['mdp']) && isset($_POST['nom']) && is
     // pour crypter (hachage) le mdp
     // $mdp = password_hash($mdp, PASSWORD_DEFAULT);
     // avec hachage du mdp, cf connexion_avec_mdp_hash.php sur le fichier de Mathieu
-    $enregistrement = $pdo->prepare("INSERT INTO membre (pseudo, mdp, nom, prenom, email, sexe, ville, cp, adresse, statut) VALUES(:pseudo, :mdp, :nom, :prenom, :email, :sexe, :ville, :cp, :adresse, 0)");
+    $enregistrement = $pdo->prepare("INSERT INTO membre (pseudo, mdp, nom, prenom, email, civilite, statut, date_enregistrement) VALUES(:pseudo, :mdp, :nom, :prenom, :email, :civilite, 0, NOW())");
     $enregistrement->bindParam(":pseudo", $pseudo, PDO::PARAM_STR);
     $enregistrement->bindParam(":mdp", $mdp, PDO::PARAM_STR);
     $enregistrement->bindParam(":nom", $nom, PDO::PARAM_STR);
     $enregistrement->bindParam(":prenom", $prenom, PDO::PARAM_STR);
     $enregistrement->bindParam(":email", $email, PDO::PARAM_STR);
-    $enregistrement->bindParam(":sexe", $sexe, PDO::PARAM_STR);
-    $enregistrement->bindParam(":ville", $ville, PDO::PARAM_STR);
-    $enregistrement->bindParam(":cp", $cp, PDO::PARAM_STR);
-    $enregistrement->bindParam(":adresse", $adresse, PDO::PARAM_STR);
+    $enregistrement->bindParam(":civilite", $civilite, PDO::PARAM_STR);
     $enregistrement->execute();
 
     // redirection sur la page connexion.php
-    header("location:connexion.php"); // /!\ les header(location:chemin) peuvent masquer des erreurs, les mettres en commentaire en phase de débug.
+    // header("location:connexion.php"); // /!\ les header(location:chemin) peuvent masquer des erreurs, les mettres en commentaire en phase de débug.
 
   }
 
@@ -150,43 +163,35 @@ echo '<pre>'; print_r($_POST); echo '</pre>';
                 <label for="pseudo">Pseudo<span style="color: red;">*</span></label>
                 <input type="text" class="form-control" name="pseudo" id="pseudo" value="<?php echo $pseudo; ?>" />
               </div>
+              
               <div class="form-group">
-                <label for="mdp">Mot de passe</label>
+                <label for="mdp">Mot de passe<span style="color: red;">*</span></label><br>
+                <span>Le mot de passe doit contenir entre 8 et 20 caractères, avoir au moins une majuscule, une minuscule et un chiffre.</span>
                 <input type="text" class="form-control" name="mdp" id="mdp" value="<?php echo $mdp; ?>" />
               </div>
               <div class="form-group">
-                <label for="nom">Nom</label>
+                <label for="nom">Nom<span style="color: red;">*</span></label>
                 <input type="text" class="form-control" name="nom" id="nom" value="<?php echo $nom; ?>" />
               </div>
               <div class="form-group">
-                <label for="prenom">Prenom</label>
+                <label for="prenom">Prenom<span style="color: red;">*</span></label>
                 <input type="text" class="form-control" name="prenom" id="prenom" value="<?php echo $prenom; ?>" />
               </div>
               <div class="form-group">
-                <label for="email">Email</label>
+                <label for="email">Email<span style="color: red;">*</span></label>
                 <input type="text" class="form-control" name="email" id="email" value="<?php echo $email; ?>" />
               </div>
               <div class="form-group">
-                <label for="sexe">Sexe</label>
-                <select class="form-control" name="sexe" id="sexe" />
+                <label for="civilite">Civilité</label>
+                <select class="form-control" name="civilite" id="civilite" />
                   <option value="m">Homme</option>
-                  <option value="f" <?php if($sexe == 'f') { echo 'selected'; } // IMPORTANT - "m" en premier, condition uniquement sur f ?> >Femme</option>
+                  <option value="f" <?php if($civilite == 'f') { echo 'selected'; } // IMPORTANT - "m" en premier, condition uniquement sur f ?> >Femme</option>
                 </select>
-              <div class="form-group">
-                <label for="ville">Ville</label>
-                <input type="text" class="form-control" name="ville" id="ville" value="<?php echo $ville; ?>" />
-              </div>
-              <div class="form-group">
-                <label for="cp">CP</label>
-                <input type="text" class="form-control" name="cp" id="cp" value="<?php echo $cp; ?>" />
-              </div>
-              <div class="form-group">
-                <label for="adresse">Adresse</label>
-                <textarea class="form-control" name="adresse" id="adresse" value=""><?php echo $adresse; // dans textarea ?></textarea>
               </div>
               <div class="form-group">
                 <input type="submit" class="form-control btn btn-success" name="inscription" id="inscription" value="Inscription" />
               </div>
+              
           </form>
 
         </div>

5857c6382ccbb470fcd7d10c5c0db66066eaa5d7
Alexandre FERNANDES <4lex.fernandes@gmail.com>
2017-07-20 17:43:02 +0200

    créationbranche alex

--- inc/init.inc.php
+++ inc/init.inc.php
@@ -3,7 +3,7 @@
 
 
 // connexion à la BDD
-$pdo = new PDO('mysql:host=localhost;dbname=wf3_lokisalle', 'root', 'root', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
+$pdo = new PDO('mysql:host=localhost;dbname=wf3_lokisalle', 'root', '', array(PDO::ATTR_ERRMODE => PDO::ERRMODE_WARNING, PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'));
 
 // appel du fichier contenant toutes les fonctions.
 require_once("function.inc.php");
@@ -20,7 +20,7 @@ session_start();
 // ---------------------------------- LIENS INTERNES AU SITE ----------------------------------
 // définition de constante pour le chemin absolu ainsi que pour la racine serveur
 // racine site
-define("URL", "/php/php_11_lokisalle/"); // modifier le repository chez moi
+define("URL", "/lokisalle/"); // modifier le repository chez moi
 
 //racine serveur - nécessaire pour les pièces jointes
 // DOCUMENT_ROOT dans $_SERVER

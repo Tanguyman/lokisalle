fb6496d1ef58bb4dd5944f7e48da50745df744b5
4lex4ndre-F <4lex.fernandes@gmail.com>
2017-07-23 19:25:24 +0200

    affichage tableau des avis - les boutons d'action ne servent à rien pour l'instant

--- admin/gestion_avis.php
+++ admin/gestion_avis.php
@@ -9,7 +9,8 @@ if(!utilisateur_est_admin())
 }
 
 
-
+// Requete sur toutes les données des tables avis, membre et salle
+$req_avis = $pdo->query("SELECT * FROM avis, membre, salle WHERE avis.id_membre = membre.id_membre AND avis.id_salle = salle.id_salle");
 
 
 // la ligne suivante commence les affichages dans la page
@@ -21,9 +22,59 @@ require("../inc/nav.inc.php");
 
         <div class="starter-template">
             <h1><span class="glyphicon glyphicon-user"></span> Gestion des avis</h1>
-            <?php //echo $message; // messages destinés à l'utilisateur ?>
-            <?= $message; // cette balise php inclue un echo, elle est équivalente à la ligne du dessus ?>
+            <?php echo $message; ?>
         </div>
+
+        <!-- tableau -->
+        <div class="row">
+            <div class="col-sm-12">
+                <table class="table table-bordered">
+                    <thead>
+                        <th>id avis</th>
+                        <th>id membre</th>
+                        <th>id salle</th>
+                        <th>commentaire</th>
+                        <th>note</th>
+                        <th>date</th>
+                        <th>action</th>
+                    </thead>
+                    <tbody>
+                        <?php
+                            while($avis = $req_avis->fetch(PDO::FETCH_ASSOC))
+                            {
+                                echo '<tr>';
+                                    //echo '<pre>'; print_r($avis); echo '</pre>';
+                                    echo '<td>' . $avis['id_avis'] . '</td>';
+                                    echo '<td>' . $avis['id_membre'] . ' - ' . $avis['email'] . '</td>';
+                                    echo '<td>' . $avis['id_salle'] . ' - ' . $avis['titre'] . '</td>';
+                                    echo '<td>' . $avis['commentaire'] . '</td>';
+                                    echo '<td>';
+                                        // transformation de la note en étoiles
+                                        $note = $avis['note'];
+                                        for($i = 0; $i < $note; $i++)
+                                        {
+                                            echo '<span class="glyphicon glyphicon-star"></span>';
+                                        }
+                                        //echo $avis['note'];
+                                    echo '</td>';
+                                    echo '<td>' . $avis['date_enregistrement'] . '</td>';
+                                    echo '<td>';
+                                        echo '<a href="?action=modif&id_avis=' . $avis['id_avis'] . '" class="btn btn-warning modif" id="id_avis=' . $avis['id_avis'] . '"><span class="glyphicon glyphicon-pencil" ></span></a>';
+                                        echo '<a href="?action=suppr&id_avis=' . $avis['id_avis'] . '" class="btn btn-danger"><span class="glyphicon glyphicon-remove"></span></a>';
+                                    echo '</td>';                                    
+                                echo '</tr>';
+                            }
+                        ?>
+                    </tbody>
+                </table>
+            </div>
+        </div><!-- fin .row -->
+        <hr />
+
+
+
+
+
 
     </div><!-- /.container -->
 
